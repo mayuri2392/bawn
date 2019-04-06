d0df01e808865996210f3f7630b64240489f81d2
mayuri <mayuripatil2392@gmail.com>
2019-04-06 13:34:53 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const User = require('./models/User');
 // Body parser middleware
 app.use(express.urlencoded({ extended: false }));
+const db = 'mongodb+srv://mayuri2305:mayuri@cluster0-mhqo2.mongodb.net/bawn?retryWrites=true';
+
+// Connect to MongoDB
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
 
 /* GET home page. */
 app.get('/', (req,res) => res.json({
 	msg: "Hello! Welcome to BAWN(Backend API with NodeJS)"
 }));
+
+//Post register
+app.post('/register', (req, res) => {
+
+	const newUser = new User({
+	    name: req.body.name,
+	    email: req.body.email,
+	    password: req.body.password
+	});
+
+	newUser
+	    .save()
+	    .then(user => res.json(user))
+	    .catch(err => res.json(err));
+});
 
 const port = process.env.PORT || 5000;
 
