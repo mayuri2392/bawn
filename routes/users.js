ed065009c6786f6d63295ce3e6076b5e07b71a1e
mayuri <mayuripatil2392@gmail.com>
2019-04-08 21:01:49 +0530

    Created routes folder

---
+++ routes/users.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const User = require('../models/User');
+const passport = require('passport');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const router = express.Router();
+
+//Post register
+router.post('/register', (req, res) => {
+
+    User.findOne({ email: req.body.email }).then(user => {
+        if (user) {
+            return res.status(400).json({ email: "Email already exists" });
+        } else {
+            const newUser = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser
+                        .save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
+                });
+            });
+        }
+    });
+});
+
+//Login
+router.post('/login', (req, res) => {
+
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    User.findOne({ email }).then(user => {
+        if (!user) {
+            return res.status(400).json({ email: "User account does not exist" });
+    
+        } else {
+            bcrypt.compare(password, user.password).then(isMatch => {
+                if (isMatch) {
+                    const payload = { id: user.id, name: user.name };
+    
+                    // Sign Token
+                    jwt.sign(
+                        payload,
+                        'worstKeptSecret', { expiresIn: 3600 },
+                        (err, token) => {
+                            res.json({
+                                success: true,
+                                token: 'Bearer ' + token,
+                                name: user.name
+                            });
+                        }
+                    );
+    
+    
+                } else {
+                    return res.status(400).json({ email: "Passwords is invalid" });
+                }
+            })
+        }
+    })
+});
+
+// Me route
+router.get('/me', passport.authenticate('jwt',{session: false}),(req, res) =>{
+    return res.json({"me": req.user})
+});
+
+
+module.exports = router;
