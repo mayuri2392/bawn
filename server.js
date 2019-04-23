c193d63c660d68be60fd256ead4baa306922ac2e
Mayuri Patil <mayuripatil2392@gmail.com>
2019-04-23 14:08:31 +0400

    added cors

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const keys = require('./config/keys');
-
+const cors = require('cors')
 // Body parser middleware
+
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const db= keys.mongoURI;
